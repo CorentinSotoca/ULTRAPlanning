3f21c20bf1c85cfbdcf7ac328c20935946726c51
corentin <corentin.sotoca.etu@univ-lille.fr>
2021-11-05 15:45:20 +0100

    Implémentation de telegram et création du README

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-ULTRAPlanning
+# ULTRAPlanning
+
+## Sommaire
+
+Petit program fait pour permettre d'organiser un peut les ICal d'HYPERPlanning pour le BUT Informatique de Villeneuve d'asq.
+
+## Installation
+
+Ce programme ne fonctionne **PAS** avec la derniére version de node. Je ne me sens pas de debugger, il faut donc utiliser la version **12.22.5**, c.f. [la page du gestionaire de version de node](https://www.npmjs.com/package/n) pour plus d'informations.
+
+Clonez le repo sur votre machine, et créez-y un fichier `config.js` dans le quel vous allez mettre vous info personnelles.
+Tout d'abord, créez vous un compte telegram, créez ensuite un bot telegram, recuperer votre **ChatId** et votre **BotToken**. Vous allez ensuite avoir besoin du **lien de l'ics**. Entrez les ensuite tel quel:
+
+```js
+module.exports = {
+    telegramBotToken: "",
+    telegramChatId: "",
+    icsLink: ""
+}
+```
+
+Vous mettrez bien sur vos informations entre les double quotes.
+
+Ensuite vous pouvez tapez ces commandes et le tour est joué !
+
+```bash
+> npm install
+> node .
+```
--- index.js
+++ index.js
@@ -1,55 +1,115 @@
+process.env.NTBA_FIX_319 = 1;
+const TelegramBot = require("node-telegram-bot-api");
 const ics = require("icalendar");
+const config = require("./config");
 let request = require("request");
 let fs = require("fs");
 
-//----------------------------Traitement d'ical---------------------------------------
-function update(){
-	request('https://calendar.google.com/calendar/ical/5jfc2090l0mh0rigmvnmd0k7j1dbnrve%40import.calendar.google.com/public/basic.ics', function(err, res, body) {
-		if(err!=null){
-			console.log("Ca a crash !")
-			return -1;
-		}
-	
-		let source = ics.parse_calendar(body); // On le parse
-		
-		// On crée les var de resultat
-		let auto = new ics.iCalendar(); // Celle ci contient toutes les autonomies
-		let resWoAuto = new ics.iCalendar(); // Et celle ci contient toutes les autres heures
-		
-		for (let i = 0; i < source.components.VEVENT.length; i++) {
-			let event = source.components.VEVENT[i]; // Creation d'une variable event ou on copie la valeur de l'ics
-			let text = event.properties.SUMMARY[0].value;
-		
-			let tiret = text.search("-");
-			if (tiret != -1) {
-				event.properties.SUMMARY[0].value = text.substring(
-					tiret + 2,
-					text.length
-				);
-				text = event.properties.SUMMARY[0].value;
-			}
-		
-			if (text.search("Autonomie") != -1) {
-				auto.addComponent(event);
+
+
+let telegramBotToken = config.telegramBotToken;
+let telegramChatId = config.telegramChatId;
+let icsLink = config.icsLink;
+
+
+// --------------- Telegram Bot ---------------
+
+let telegram = new TelegramBot(telegramBotToken);
+function logToTelegram() {
+	const msg = Object.values(arguments)
+		.map((msg) => {
+			if (typeof msg === "object") {
+				return msg.stack ? msg.stack : JSON.stringify(msg);
 			} else {
-				resWoAuto.addComponent(event);
+				return String(msg);
+			}
+		})
+		.join(" ");
+	return telegram.sendMessage(telegramChatId, msg);
+}
+
+console.notify = function () {
+	try {
+		console.log(arguments);
+		logToTelegram(arguments).catch(console.error);
+	} catch (e) {
+		console.error(e);
+	}
+};
+
+const standardErrorLog = console.error.bind(console);
+console.error = function () {
+	try {
+		console.log("Une erreur a été envoyée sur telegram !");
+		logToTelegram("⚠️", arguments).catch(console.error);
+	} catch (e) {
+		console.error(e);
+	}
+};
+
+//----------------------------Traitement d'ical---------------------------------------
+function update() {
+	request(
+		icsLink,
+		function (err, res, body) {
+			if (err != null) {
+				console.error(err);
+				return -1;
+			}
+
+			let source
+
+			try {
+				source = ics.parse_calendar(body); // On le parse
+			} catch (error) {
+				console.error(error);
+				return
+			}
+
+			// On crée les var de resultat
+			let auto = new ics.iCalendar(); // Celle ci contient toutes les autonomies
+			let resWoAuto = new ics.iCalendar(); // Et celle ci contient toutes les autres heures
+
+			for (let i = 0; i < source.components.VEVENT.length; i++) {
+				let event = source.components.VEVENT[i]; // Creation d'une variable event ou on copie la valeur de l'ics
+				let text = event.properties.SUMMARY[0].value;
+
+				let tiret = text.search("-");
+				if (tiret != -1) {
+					event.properties.SUMMARY[0].value = text.substring(
+						tiret + 2,
+						text.length
+					);
+					text = event.properties.SUMMARY[0].value;
+				}
+
+				if (text.search("Autonomie") != -1) {
+					auto.addComponent(event);
+				} else {
+					resWoAuto.addComponent(event);
+				}
+			}
+
+			try {
+				const data = fs.writeFileSync(
+					"result/resWoAuto.ics",
+					resWoAuto
+				);
+				//file written successfully
+			} catch (err) {
+				console.error(err);
+			}
+
+			try {
+				const data = fs.writeFileSync("result/auto.ics", auto);
+				//file written successfully
+			} catch (err) {
+				console.error(err);
 			}
 		}
-		console.log(resWoAuto)
-		try {
-			const data = fs.writeFileSync("result/resWoAuto.ics", resWoAuto);
-			//file written successfully
-		} catch (err) {
-			console.error(err);
-		}
-		
-		try {
-			const data = fs.writeFileSync("result/auto.ics", auto);
-			//file written successfully
-		} catch (err) {
-			console.error(err);
-		}
-	});
+	);
 }
+
+logToTelegram("UltraPlanning viens de demarrer !")
 update();
-setInterval(update,3600000)
+setInterval(update, 3600000);
