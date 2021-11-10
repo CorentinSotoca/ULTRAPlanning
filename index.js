6676f42b07a85cef731c919d2a2f671f61263c6b
corentin <corentin.sotoca.etu@univ-lille.fr>
2021-11-10 11:41:14 +0100

    Add telegram fonctionnalities

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 process.env.NTBA_FIX_319 = 1;
 const TelegramBot = require("node-telegram-bot-api");
-const os=require('os')
+const os = require("os");
 const ics = require("icalendar");
 const config = require("./config");
 let request = require("request");
 let fs = require("fs");
 
-
-
 let telegramBotToken = config.telegramBotToken;
 let telegramChatId = config.telegramChatId;
 let icsLink = config.icsLink;
 
-
 // --------------- Telegram Bot ---------------
 
-let telegram = new TelegramBot(telegramBotToken);
+let telegram = new TelegramBot(telegramBotToken, { polling: true });
 function logToTelegram() {
 	const msg = Object.values(arguments)
 		.map((msg) => {
@@ -48,97 +45,116 @@ console.error = function () {
 	}
 };
 
+//-----------------------------Commande-----------------------------------------------
+
+telegram.on("message", (msg) => {
+	if (msg.text == "/status") {
+		var uptime = process.uptime()+60;
+		const date = new Date(uptime * 1000);
+		const days = date.getUTCDate() - 1,
+			hours = date.getUTCHours(),
+			minutes = date.getUTCMinutes();
+
+
+		let segments = [];
+
+		if (days > 0) segments.push(days + " day" + (days == 1 ? "" : "s"));
+		if (hours > 0) segments.push(hours + " hour" + (hours == 1 ? "" : "s"));
+		if (minutes > 0)
+			segments.push(minutes + " minute" + (minutes == 1 ? "" : "s"));
+		
+		const dateString = segments.join(", ");
+
+		logToTelegram("🤖 ULTRAPlanning est UP depuis", dateString);
+	}
+
+	else if(msg.text=="/update"){
+		update();
+		logToTelegram("🤖 Les ICALs ont bien été mis à jour !")
+	}
+});
+
 //----------------------------Traitement d'ical---------------------------------------
 function update() {
-	request(
-		icsLink,
-		function (err, res, body) {
-			if (err != null) {
-				console.error(err);
-				return -1;
-			}
+	request(icsLink, function (err, res, body) {
+		if (err != null) {
+			console.error(err);
+			return -1;
+		}
 
-			let source
+		let source;
 
-			try {
-				source = ics.parse_calendar(body); // On le parse
-			} catch (error) {
-				console.error(error);
-				return
-			}
+		try {
+			source = ics.parse_calendar(body); // On le parse
+		} catch (error) {
+			console.error(error);
+			return;
+		}
 
-			// On crée les var de resultat
-			let auto = new ics.iCalendar(); // Celle ci contient toutes les autonomies
-			let controle = new ics.iCalendar(); // DS, CTP et interro
-			let amphi = new ics.iCalendar(); // Amphi
-			let tdTp = new ics.iCalendar(); // Et celle ci contient toutes les autres heures
+		// On crée les var de resultat
+		let auto = new ics.iCalendar(); // Celle ci contient toutes les autonomies
+		let controle = new ics.iCalendar(); // DS, CTP et interro
+		let amphi = new ics.iCalendar(); // Amphi
+		let tdTp = new ics.iCalendar(); // Et celle ci contient toutes les autres heures
 
+		for (let i = 0; i < source.components.VEVENT.length; i++) {
+			let event = source.components.VEVENT[i]; // Creation d'une variable event ou on copie la valeur de l'ics
+			let text = event.properties.SUMMARY[0].value;
 
-			for (let i = 0; i < source.components.VEVENT.length; i++) {
-				let event = source.components.VEVENT[i]; // Creation d'une variable event ou on copie la valeur de l'ics
-				let text = event.properties.SUMMARY[0].value;
-
-				let tiret = text.search("-");
-				if (tiret != -1) {
-					event.properties.SUMMARY[0].value = text.substring(
-						tiret + 2,
-						text.length
-					);
-					text = event.properties.SUMMARY[0].value;
-				}
-
-				// console.log(event.properties.LOCATION[0].value)
-				if (text.search("Autonomie") != -1) {
-					auto.addComponent(event);
-				}else if(event.properties.LOCATION[0].value.includes("1A02")){
-					controle.addComponent(event);
-				} else if(event.properties.LOCATION[0].value.includes("1A") || event.properties.LOCATION[0].value.includes("Amphis")){
-					amphi.addComponent(event);
-				}else {
-					tdTp.addComponent(event);
-				}
-			}
-
-			try {
-				const data = fs.writeFileSync(
-					"result/tdTp.ics",
-					tdTp
+			let tiret = text.search("-");
+			if (tiret != -1) {
+				event.properties.SUMMARY[0].value = text.substring(
+					tiret + 2,
+					text.length
 				);
-				//file written successfully
-			} catch (err) {
-				console.error(err);
+				text = event.properties.SUMMARY[0].value;
 			}
 
-			try {
-				const data = fs.writeFileSync("result/auto.ics", auto);
-				//file written successfully
-			} catch (err) {
-				console.error(err);
-			}
-
-			try {
-				const data = fs.writeFileSync(
-					"result/amphi.ics",
-					amphi
-				);
-				//file written successfully
-			} catch (err) {
-				console.error(err);
-			}
-
-			try {
-				const data = fs.writeFileSync(
-					"result/controle.ics",
-					controle
-				);
-				//file written successfully
-			} catch (err) {
-				console.error(err);
+			// console.log(event.properties.LOCATION[0].value)
+			if (text.search("Autonomie") != -1) {
+				auto.addComponent(event);
+			} else if (event.properties.LOCATION[0].value.includes("1A02")) {
+				controle.addComponent(event);
+			} else if (
+				event.properties.LOCATION[0].value.includes("1A") ||
+				event.properties.LOCATION[0].value.includes("Amphis")
+			) {
+				amphi.addComponent(event);
+			} else {
+				tdTp.addComponent(event);
 			}
 		}
-	);
+
+		try {
+			const data = fs.writeFileSync("result/tdTp.ics", tdTp);
+			//file written successfully
+		} catch (err) {
+			console.error(err);
+		}
+
+		try {
+			const data = fs.writeFileSync("result/auto.ics", auto);
+			//file written successfully
+		} catch (err) {
+			console.error(err);
+		}
+
+		try {
+			const data = fs.writeFileSync("result/amphi.ics", amphi);
+			//file written successfully
+		} catch (err) {
+			console.error(err);
+		}
+
+		try {
+			const data = fs.writeFileSync("result/controle.ics", controle);
+			//file written successfully
+		} catch (err) {
+			console.error(err);
+		}
+	});
 }
 
-logToTelegram("🤖 UltraPlanning viens de demarrer depuis", os.hostname()," !")
+logToTelegram("🤖 UltraPlanning viens de demarrer depuis", os.hostname(), " !");
 update();
 setInterval(update, 3600000);
